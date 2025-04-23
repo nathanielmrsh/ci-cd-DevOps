c9930dbffb9588a4f648493f52c0e1c910abb0ac
Nathan Marsh <113995325+nathanielmrsh@users.noreply.github.com>
2025-04-23 13:38:57 +0100

    Delete ProductSite2-master/ProductSite2-master directory

--- ProductSite2-master/ProductSite2-master/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProductSite2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
